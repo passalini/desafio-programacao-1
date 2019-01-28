1b1ae35536afd9f36e5cf742ac0c5adf82033a71
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2019-01-28 04:18:26 -0200

    add more tests and add logout button

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -11,7 +11,7 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get reports_url
     assert_response :success
-    assert_select '#reports .report', count: 1
+    assert_select '#reports .report', count: 2
     assert_select "#reports #report-#{@report.id} .income", text: '$95.00'
   end
 
